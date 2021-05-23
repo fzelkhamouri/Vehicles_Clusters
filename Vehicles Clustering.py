d473f7fdd2497df77da4b194f87bc2d94aa5d873
fzelkhamouri <32655033+fzelkhamouri@users.noreply.github.com>
2021-05-24 00:51:56 +0100

    Code

---
+++ Vehicles Clustering.py
@@ -0,0 +1,134 @@
+import numpy as np 
+import pandas as pd
+from scipy import ndimage 
+from scipy.cluster import hierarchy 
+from scipy.spatial import distance_matrix 
+from matplotlib import pyplot as plt 
+from sklearn.cluster import AgglomerativeClustering 
+%matplotlib inline
+
+##### Read the Data
+filename = 'D:\\cars_clus.csv'
+
+#Read csv
+pdf = pd.read_csv(filename)
+print ("Shape of dataset: ", pdf.shape)
+print(pdf.head(5))
+
+##### Data Clearning
+
+print ("Shape of dataset before cleaning: ", pdf.size)
+pdf[[ 'sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']] = pdf[['sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']].apply(pd.to_numeric, errors='coerce')
+pdf = pdf.dropna()
+pdf = pdf.reset_index(drop=True)
+print ("Shape of dataset after cleaning: ", pdf.size)
+print(pdf.head(5))
+
+###### Feature selection
+
+featureset = pdf[['engine_s',  'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap', 'mpg']]
+
+###### Normalization
+
+from sklearn.preprocessing import MinMaxScaler
+x = featureset.values #returns a numpy array
+min_max_scaler = MinMaxScaler()
+feature_mtx = min_max_scaler.fit_transform(x)
+print(feature_mtx [0:5])
+print('----------------')
+###### Clustering using numpy
+
+import scipy
+leng = feature_mtx.shape[0]
+D = np.zeros([leng,leng])
+for i in range(leng):
+    for j in range(leng):
+        D[i,j] = scipy.spatial.distance.euclidean(feature_mtx[i], feature_mtx[j])
+print(D)
+
+import pylab
+import scipy.cluster.hierarchy
+Z = hierarchy.linkage(D, 'complete')
+
+from scipy.cluster.hierarchy import fcluster
+max_d = 3
+clusters = fcluster(Z, max_d, criterion='distance')
+print(clusters)
+
+# Also, you can determine the number of clusters directly:
+
+from scipy.cluster.hierarchy import fcluster
+k = 5
+clusters = fcluster(Z, k, criterion='maxclust')
+print(clusters)
+
+# Now, plot the dendrogram:
+    
+fig = pylab.figure(figsize=(18,50))
+def llf(id):
+    return '[%s %s %s]' % (pdf['manufact'][id], pdf['model'][id], int(float(pdf['type'][id])) )
+    
+dendro = hierarchy.dendrogram(Z,  leaf_label_func=llf, leaf_rotation=0, leaf_font_size =12, orientation = 'right')
+
+###### Clustering using scikit-learn
+
+from sklearn.metrics.pairwise import euclidean_distances
+dist_matrix = euclidean_distances(feature_mtx,feature_mtx) 
+print(dist_matrix)
+
+Z_using_dist_matrix = hierarchy.linkage(dist_matrix, 'complete')
+
+fig = pylab.figure(figsize=(18,50))
+def llf(id):
+    return '[%s %s %s]' % (pdf['manufact'][id], pdf['model'][id], int(float(pdf['type'][id])) )
+    
+dendro = hierarchy.dendrogram(Z_using_dist_matrix,  leaf_label_func=llf, leaf_rotation=0, leaf_font_size =12, orientation = 'right')
+
+agglom = AgglomerativeClustering(n_clusters = 6, linkage = 'complete')
+agglom.fit(dist_matrix)
+
+agglom.labels_
+
+pdf['cluster_'] = agglom.labels_
+print(pdf.head())
+
+import matplotlib.cm as cm
+n_clusters = max(agglom.labels_)+1
+colors = cm.rainbow(np.linspace(0, 1, n_clusters))
+cluster_labels = list(range(0, n_clusters))
+
+# Create a figure of size 6 inches by 4 inches.
+plt.figure(figsize=(16,14))
+
+for color, label in zip(colors, cluster_labels):
+    subset = pdf[pdf.cluster_ == label]
+    for i in subset.index:
+            plt.text(subset.horsepow[i], subset.mpg[i],str(subset['model'][i]), rotation=25) 
+    plt.scatter(subset.horsepow, subset.mpg, s= subset.price*10, c=color, label='cluster'+str(label),alpha=0.5)
+#    plt.scatter(subset.horsepow, subset.mpg)
+plt.legend()
+plt.title('Clusters')
+plt.xlabel('horsepow')
+plt.ylabel('mpg')
+
+pdf.groupby(['cluster_','type'])['cluster_'].count()
+agg_cars = pdf.groupby(['cluster_','type'])['horsepow','engine_s','mpg','price'].mean()
+print(agg_cars)
+
+plt.figure(figsize=(16,10))
+for color, label in zip(colors, cluster_labels):
+    subset = agg_cars.loc[(label,),]
+    for i in subset.index:
+        plt.text(subset.loc[i][0]+5, subset.loc[i][2], 'type='+str(int(i)) + ', price='+str(int(subset.loc[i][3]))+'k')
+    plt.scatter(subset.horsepow, subset.mpg, s=subset.price*20, c=color, label='cluster'+str(label))
+plt.legend()
+plt.title('Clusters')
+plt.xlabel('horsepow')
+plt.ylabel('mpg')
+
+
+
